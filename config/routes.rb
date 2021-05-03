173d684d6d86236437e8173ea24be9fee50109b1
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-03 09:18:28 +0900

    devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, controllers: {
+    passwords:          'users/passwords',
+    registrations:      'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions:           'users/sessions',
+    confirmations:      "users/confirmations"
+  }
+
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords'
+  }
+
   root 'static_pages#home'
 end
