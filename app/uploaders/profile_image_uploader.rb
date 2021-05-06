476bbd74525235fdcee8c980260993d1060f5443
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-07 00:34:01 +0900

    fix

--- app/uploaders/profile_image_uploader.rb
+++ app/uploaders/profile_image_uploader.rb
@@ -28,9 +28,13 @@ class ProfileImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :icon do
+    process resize_to_fit: [100, 100]
+  end
+
+  version :thumb do
+    process resize_to_fit: [400, 400]
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
