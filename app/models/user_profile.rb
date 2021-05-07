11bb047b3a6b95308e8be2cff612cccda9d345da
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-07 15:49:42 +0900

    fix

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -7,6 +7,6 @@ class UserProfile < ApplicationRecord
 
   mount_uploader :image, ProfileImageUploader
 
-  enum sex: { men: 0, wemen: 1, other: 2 }
-  enum job: { student: 0, bank: 1, nurse: 2 }
+  enum sex: { men: 0, women: 1, other: 2 }
+  enum job: { student: 0, banker: 1, nurse: 2 }
 end
