81be3e97741a97d826b68f9b433e3f38ed4b061e
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-08 00:38:12 +0900

    更新

--- app/controllers/user/user_profiles_controller.rb
+++ app/controllers/user/user_profiles_controller.rb
@@ -8,5 +8,22 @@ class User::UserProfilesController < ApplicationController
 
   def update
     @user_profile = current_user.user_profile
+    if @user_profile.update(
+      name: user_profile_params[:name],
+      sex: user_profile_params[:sex],
+      age: user_profile_params[:age],
+      job: user_profile_params[:job],
+    )
+      flash[:success] = "更新が完了しました。"
+      redirect_to edit_user_profile_path
+    else
+      flash[:danger] = "更新に失敗しました。"
+      edit_user_profile_path
+    end
   end
+
+  private
+    def user_profile_params
+      params.require(:user_profile).permit(:name, :sex, :age, :job, :image, :introduction)
+    end
 end
