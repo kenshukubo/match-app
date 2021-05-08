5422f7c59150b4aaba471af264073f931184db2b
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-09 02:40:58 +0900

    bell

--- app/controllers/user/post_members_controller.rb
+++ app/controllers/user/post_members_controller.rb
@@ -1,5 +1,9 @@
 class User::PostMembersController < ApplicationController
 
   def new
+    unless current_user.post.present?
+      redirect_to root_path 
+      flash[:alert] = "募集を作成しましょう"
+    end
   end
 end
