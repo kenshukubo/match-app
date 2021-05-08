2495a08eaeee583a62b33dfffab8a9c277119819
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-08 21:04:46 +0900

    post 作成フォーム

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -1,7 +1,19 @@
 class User::PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.build_post(post_params)
+    if @post.save
+      redirect_to new_post_path, notice: 'メンバーを選びましょう'
+    else
+      redirect_to new_post_path, alert: '作成に失敗しました'
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:user_id, :place, :number, :time, :detail)
+    end
 end
