4403afeb7ad9ba7552f49ef9aed6cfbf8838a570
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-08 22:56:44 +0900

    posts update destroy

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -12,6 +12,35 @@ class User::PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = current_user.post
+  end
+
+  def update
+    @post = current_user.post
+    if @post.update!(
+      place: post_params[:place],
+      number: post_params[:number],
+      time: post_params[:time],
+      detail: post_params[:detail],
+    )
+      flash[:notice] = "更新完了！"
+      redirect_to root_path
+    else
+      flash[:alert] = "更新に失敗しました"
+      redirect_to edit_post_path(@post.id)
+    end
+  end
+
+  def destroy
+    @post = current_user.post
+    if @post.destroy
+      redirect_to root_path, notice: '募集を削除しました'
+    else
+      redirect_to new_post_path, alert: '削除に失敗しました'
+    end
+  end
+
   private
     def post_params
       params.require(:post).permit(:user_id, :place, :number, :time, :detail)
