77468fad1b2639340a66e8509dcf8dc97a558f00
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-10 23:35:27 +0900

    fix

--- app/models/user_notification.rb
+++ app/models/user_notification.rb
@@ -2,4 +2,9 @@ class UserNotification < ApplicationRecord
   belongs_to :user
 
   validates :unchecked_num, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def add_unchecked_notification_count
+    self.unchecked_num += 1
+    self.save!
+  end
 end
