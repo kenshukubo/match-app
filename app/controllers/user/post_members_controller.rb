7b384e0a7eab9e6f8a34b76b92f4209471acb9b1
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-11 01:47:41 +0900

    edit 参加不参加

--- app/controllers/user/post_members_controller.rb
+++ app/controllers/user/post_members_controller.rb
@@ -18,6 +18,48 @@ class User::PostMembersController < ApplicationController
     .where(post_members: {is_confirmed: true})
   end
 
+  def edit
+    @post_member = PostMember.find(params[:id])
+    @post = @post_member.post
+  end
+
+  def update
+    @post_member = PostMember.find(params[:id])
+    @post = @post_member.post
+    begin
+      ActiveRecord::Base.transaction do
+        @post_member.update(is_confirmed: post_member_params[:is_confirmed])
+
+        host_user_id = @post.user_id
+        username = current_user.user_profile.name
+
+        if post_member_params[:is_confirmed] == "true"
+          category = "attend"
+          message = "#{username}さんは参加を希望しました"
+        else
+          category = "not_attend"
+          message = "#{username}さんは残念ながら参加できません"
+        end
+
+        Notification.create!(
+          target_user_id: host_user_id,
+          message: message,
+          category: category,
+          url: post_path(@post.id)
+        )
+
+        user_notification = UserNotification.find_by(user_id: host_user_id)
+        user_notification.add_unchecked_notification_count
+      end
+      flash[:notice] = "更新完了！"
+      redirect_to root_path
+    rescue => error
+      p error
+      flash[:alert] = "更新に失敗しました"
+      redirect_to edit_post_member_path(@post_member.id)
+    end
+  end
+
   def destroy
     @invited_member = PostMember.find(params[:id])
     if @invited_member.destroy
@@ -26,4 +68,9 @@ class User::PostMembersController < ApplicationController
       redirect_to new_post_member_path, alert: '招待キャンセルに失敗しました'
     end
   end
+
+  private
+    def post_member_params
+      params.require(:post_member).permit(:is_confirmed)
+    end
 end
