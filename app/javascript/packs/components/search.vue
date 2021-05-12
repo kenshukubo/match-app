a3f5712d185969dc3dde2712d517adb6b89625ff
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-13 00:45:46 +0900

    modal header

--- app/javascript/packs/components/search.vue
+++ app/javascript/packs/components/search.vue
@@ -6,7 +6,13 @@
 
     <div class="search-modal" v-show="showSearchModal">
       <div class="search-modal-container">
-        <input type="text">
+        <div class="search-area-header">
+          <div class="search-area-wrapper">
+            <input type="text" placeholder="メールアドレスや名前で検索" class="search-area">
+            <img :src="searchImage" class="search-img modal-search-img">
+          </div>
+          <img :src="closeImage" class="close-img" @click="closeSearchModal">
+        </div>
       </div>
     </div>
   </div>
@@ -14,18 +20,23 @@
 <script>
 import axios from 'packs/axios'
 import searchImage from 'packs/assets/images/search.png'
+import closeImage from 'packs/assets/images/close.png'
 
 export default {
   data() {
     return {
       searchImage,
+      closeImage,
       showSearchModal: false,
     }
   },
   methods: {
     openSearchModal: function(){
       this.showSearchModal = true;
-    }
+    },
+    closeSearchModal: function(){
+      this.showSearchModal = false;
+    },
   }
 }
 </script>
@@ -39,6 +50,7 @@ export default {
 .search-img{
   cursor: pointer;
   width: 20px;
+  height: 20px;
 }
 
 .search-modal{
@@ -54,5 +66,38 @@ export default {
 }
 .search-modal-container{
   max-width: 600px;
+  width: 100%;
+}
+
+.search-area-header{
+  display: flex;
+  align-items: center;
+  padding: 4px;
+}
+
+.search-area-wrapper{
+  position: relative;
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.search-area{
+  width: 100%;
+  min-height: 40px;
+  padding: 4px 8px;
+  font-size: 14px;
+  padding-right: 40px!important;
+}
+
+.modal-search-img{
+  padding: 8px;
+  position: absolute;
+  right: 12px;
+}
+.close-img{
+  width: 20px;
+  height: 20px;
+  padding: 8px;
 }
 </style>
