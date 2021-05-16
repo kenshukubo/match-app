d8091139fc91dfc1eb24acbc9e514426eda0cc1d
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-17 00:04:43 +0900

    index 画像

--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -1,21 +1,78 @@
 <template>
   <div>
-    
+    <div class="post-list">
+      <template v-for="(post, index) in postList">
+        <div class="post-item-wrapper" :key="`post-${index}`">
+          <span class="post-created">{{post.createdAt}}</span>
+          <div class="post-item"
+            :style="{ 'background-image': 'url(' + postBackgroundImage + ')' }"
+          >
+            <div class="post-item__text-area">
+
+            </div>
+          </div>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 <script>
 import axios from 'packs/axios'
+import postBackgroundImage from 'packs/assets/images/post-background.png'
 export default {
   data() {
-
+    return{
+      postBackgroundImage,
+      postList: "",
+    }
   },
   created() {
-
+    this.fetchPostList();
   },
   methods: {
-
+    async fetchPostList(){
+      var self = this;
+      try {
+        const res = await axios.get("/api/v1/posts")
+        self.postList = res.data.postList;
+      } catch(e) {
+        console.log(e)
+      }
+    },
   }
 }
 </script>
 <style>
+.post-item-wrapper{
+  margin: 24px 0;
+  padding: 16px 16px 4px;
+  background: #fff;
+  box-shadow: 2px 2px 8px -2px rgb(0 0 0 / 5%);
+}
+
+.post-created{
+  display: inline-block;
+  padding: 0 8px;
+  border-radius: 10px;
+  background:#001858;
+  color: #fff;
+  font-size: .571rem;
+  line-height: 1.428rem;
+  margin-bottom: 4px;
+}
+
+.post-item{
+  display: flex;
+  justify-content: center;
+  background-size: cover;
+  height: 320px;
+}
+
+.post-item__text-area{
+  width: 90%;
+  height: 72%;
+  margin-top: 20px;
+  background: #fff;
+  border-radius: 8px;
+}
 </style>
---
+++ app/views/api/v1/posts/index.json.jbuilder
@@ -0,0 +1,9 @@
+json.postList do
+  json.array!(@posts) do |post|
+    json.place      post.place
+    json.time       post.time
+    json.number     post.number
+    json.detail     post.detail
+    json.createdAt  post.created_at.strftime("%-m月%-d日 %-H時%-M分")
+  end
+end
