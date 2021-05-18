fa283134630cb97014bffa570f40e20d5c4a40e7
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-19 01:44:59 +0900

    fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters
 
   # GET /resource/sign_up
   # def new
@@ -50,7 +51,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:sex])
+    end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
