919979ee974019d949a1a10f2e3c5eb21b623780
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-05-22 17:59:21 +0900

    アタック隊フォーム

--- app/controllers/user/attack_groups_controller.rb
+++ app/controllers/user/attack_groups_controller.rb
@@ -1,5 +1,15 @@
 class User::AttackGroupsController < ApplicationController
   def new
-    @attack_group = current_user.attack_groups
+    @attackers = Attacker.new
+    @host_friends = current_user.friend_users
+  end
+
+  def create
+    begin
+      ActiveRecord::Base.transaction do
+      end
+    rescue => error
+      p error
+    end
   end
 end
