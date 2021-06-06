5bfd088fbce0ef8cbc06ac5fc2657f6ce3ebdc64
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-06-07 08:06:35 +0900

    fix

--- app/controllers/api/v1/search_users_controller.rb
+++ app/controllers/api/v1/search_users_controller.rb
@@ -2,8 +2,9 @@ class Api::V1::SearchUsersController < Api::ApplicationController
   def index
     @keyword = params[:keyword]
     @searched_users = User
-    .includes(:user_profile, :relationships)
+    .includes(:user_profile, post: :post_members)
     .where.not(id: current_user.id)
+    .where.not(id: current_user.post.post_members.pluck(:user_id))
     .where.not(id: current_user.friend_users.pluck(:id))
     .same_sex(current_user)
     .search_by_keyword(@keyword)
