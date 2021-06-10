c82955997c4032d99cac599d6cb0da6bed59d373
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-06-10 10:09:40 +0900

    fix

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::PostsController < Api::ApplicationController
 
     return if !current_user
 
+    #TODO 開始日時が切れたやつを除く
     @posts = Post
     .includes(:post_members, :user)
     .where.not(users: {sex: current_user.sex})
