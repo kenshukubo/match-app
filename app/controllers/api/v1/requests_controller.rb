80aff4337ca021284f5c633615b7fc067b70823b
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-06-12 23:57:56 +0900

    fix

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -21,7 +21,7 @@ class Api::V1::RequestsController < Api::ApplicationController
 
         # 通知作成
         category = "request"
-        message = "あなたの募集にアタックされました"
+        message = "あなたの募集がアタックされました"
 
         post_member_ids.each do |user_id|
           Notification.create!(
