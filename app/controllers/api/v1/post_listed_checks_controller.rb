ba380853703e9b0aa3fc38c163b1f825bcac43bd
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-06-14 10:06:54 +0900

    fix

--- app/controllers/api/v1/post_listed_checks_controller.rb
+++ app/controllers/api/v1/post_listed_checks_controller.rb
@@ -1,15 +1,14 @@
 class Api::V1::PostListedChecksController < Api::ApplicationController
   def update
-    return if !current_user
-    @post = Post.find_by(user: current_user)
-
-    return if @post.blank?
-
-    if @post.is_modal
+    return if Post.find_by(user: current_user).blank?
+    return if !current_user 
+    
+    post = Post.find_by(user: current_user)
+    if post.is_modal
       render json: { isModal: false }
     else
-      @post.update!( is_modal: true ) if @post.all_member_here?
-      render json: { isModal: @post.is_modal }
+      post.update!( is_modal: true ) if post.all_member_here?
+      render json: { isModal: post.is_modal }
     end
   end
 end
