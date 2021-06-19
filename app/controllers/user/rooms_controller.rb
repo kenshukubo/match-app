44ac786f1d79fe5fcbb3c9101483478af42fc809
kenshukubo <kensyu124logic@yahoo.co.jp>
2021-06-19 10:30:45 +0900

    fix

--- app/controllers/user/rooms_controller.rb
+++ app/controllers/user/rooms_controller.rb
@@ -1,14 +1,6 @@
 class User::RoomsController < ApplicationController
-  before_action :set_layout, only: [:show]
 
   def show
     @user = current_user
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-
-    def set_layout
-      render :layout => 'chat'
-    end
 end
